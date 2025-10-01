255add06a03235c2f1fa114cc592ae8c509b16ce
naanayoung <131951552+naanayoung@users.noreply.github.com>
2025-10-01 15:51:15 +0900

    Delete main frontend (after single-broker push)

--- frontend/src/pages/HomePage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function HomePage(){
-	return <h1> This is HOMEPAGE</h1>;
-}
-
-export default HomePage;
--- frontend/src/pages/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NotFound(){
-	return <h1>404 - Can not found Page.</h1>;
-}
-
-export default NotFound;
