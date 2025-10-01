7cab8a31946b326da72920e5041521317fe8f3c6
naanayoung <131951552+naanayoung@users.noreply.github.com>
2025-10-01 15:50:51 +0900

    Delete main backend(after single-broker push)

--- backend/coupons/admin.py
+++
@@ -1,9 +0,0 @@
-"""관리자 페이지"""
-
-from django.contrib import admin
-from .models import Coupon
-
-@admin.register(Coupon)
-class CouponAdmin(admin.ModelAdmin):
-    list_display = ['code', 'user', 'event', 'issued_at', 'is_used']
-    search_fields = ['code']
